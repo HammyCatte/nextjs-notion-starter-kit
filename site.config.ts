1cb7166d60fe2f9fcd1d12cc51b26668953ae2f2
HammyCatte <200344156+HammyCatte@users.noreply.github.com>
2025-02-23 19:33:24 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '15542883e81a8049a566d9d9225b8a06',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '测试中~',
+  domain: 'www.HammyCatte.top',
+  author: 'HammyCatte',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
